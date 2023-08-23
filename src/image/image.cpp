76969d9ecf7a28411674640723fc028018d1e4ae
piroot01 <piroot01@gmail.com>
2023-08-23 21:52:10 +0200

    Edited image.cpp

---
+++ src/image/image.cpp
@@ -0,0 +1,51 @@
+#include "image/image.h"
+
+
+Image::Image() :
+    Image(std::string("output.bmp"), 256, 256)
+{
+    initializeImage();
+}
+
+
+Image::Image(const uint32_t width, const uint32_t height) :
+    Image(std::string("output.bmp"), width, height)
+{
+    initializeImage();
+}
+
+
+Image::Image(const std::string& fileName, const uint32_t width, const uint32_t height) :
+    _fileName(fileName),
+    _width(width),
+    _height(height),
+    _bmpHeader(_width, _height),
+    _bmpInfoHeader(_width, _height)
+{
+    initializeImage();
+}
+
+
+Image::~Image()
+{
+    _image->close();
+}
+
+
+void Image::generateHeader()
+{
+    _image->writeHeader(_bmpHeader, _bmpInfoHeader);
+}
+
+
+void Image::appendPixel(const Color& color)
+{
+    _image->writePixel(color);
+}
+
+
+void Image::initializeImage()
+{
+    _image = new ImageWriter(_fileName);
+    _image->open();
+}
