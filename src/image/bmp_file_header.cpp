a6aa27a66f507bb692a6d7923fdeb52c7b544b63
piroot01 <piroot01@gmail.com>
2023-08-23 21:51:34 +0200

    Edited bmp headers

---
+++ src/image/bmp_file_header.cpp
@@ -0,0 +1,32 @@
+#include "image/bmp_file_header.h"
+#include "image/bmp_header_size.h"
+#include <cstdint>
+
+
+BmpFileHeader::BmpFileHeader(uint32_t width, uint32_t height) :
+    _sizeOfBitmapFile(width * height + BMP_FILE_HEADER_SIZE + BMP_INFO_HEADER_SIZE),
+    _pixelDataOffset(BMP_FILE_HEADER_SIZE + BMP_INFO_HEADER_SIZE)
+{
+}
+
+std::array<unsigned char, BMP_FILE_HEADER_SIZE> BmpFileHeader::get() const
+{
+    std::array<unsigned char, BMP_FILE_HEADER_SIZE> bmpFileHeader = {};
+    bmpFileHeader[0] = _bitmapSignatureBytes[0];
+    bmpFileHeader[1] = _bitmapSignatureBytes[1];
+    bmpFileHeader[2] = static_cast<unsigned char>(_sizeOfBitmapFile);
+    bmpFileHeader[3] = static_cast<unsigned char>(_sizeOfBitmapFile >> 8);
+    bmpFileHeader[4] = static_cast<unsigned char>(_sizeOfBitmapFile >> 16);
+    bmpFileHeader[5] = static_cast<unsigned char>(_sizeOfBitmapFile >> 24);
+    bmpFileHeader[10] = static_cast<unsigned char>(_pixelDataOffset);
+    return bmpFileHeader;
+}
+
+BmpFileHeader& BmpFileHeader::operator=(const BmpFileHeader& bmpHeader)
+{
+    if (this == &bmpHeader)
+        return *this;
+    _sizeOfBitmapFile = bmpHeader._sizeOfBitmapFile;
+    _pixelDataOffset = bmpHeader._pixelDataOffset;
+    return *this;
+}
--- src/image/bmp_info_header.cpp
+++ src/image/bmp_info_header.cpp
@@ -1,18 +1,31 @@
 #include "image/bmp_info_header.h"
-#include "image/bmp_header_size.h"
-#include <cstdint>
 
-BmpInfoHeader::BmpInfoHeader(uint32_t width, uint32_t height, uint32_t horizontalResolution = 3780, uint32_t verticalResolution = 3780) :
-    _sizeOfThisHeader(BMP_INFO_HEADER_SIZE),
+
+BmpInfoHeader::BmpInfoHeader(uint32_t width, uint32_t height) :
     _width(width),
     _height(height),
     _numberOfColorPlanes(1),
-    _colorDepth(24),
-    _compressionMethod(0),
-    _rawBitmapDataSize(0),
-    _horizontalResolution(horizontalResolution),
-    _verticalResolution(verticalResolution),
-    _colorTableEntries(0),
-    _importantColors(0)
+    _bitsPerPixel(3 * 8)
 {
 }
+
+
+std::array<unsigned char, BMP_INFO_HEADER_SIZE> BmpInfoHeader::get() const
+{
+    std::array<unsigned char, BMP_INFO_HEADER_SIZE> bmpInfoHeader = {};
+    bmpInfoHeader[0] = static_cast<unsigned char>(BMP_INFO_HEADER_SIZE);
+    bmpInfoHeader[4] = static_cast<unsigned char>(_width);
+    bmpInfoHeader[5] = static_cast<unsigned char>(_width >> 8);
+    bmpInfoHeader[6] = static_cast<unsigned char>(_width >> 16);
+    bmpInfoHeader[7] = static_cast<unsigned char>(_width >> 24);
+    bmpInfoHeader[8] = static_cast<unsigned char>(_height);
+    bmpInfoHeader[9] = static_cast<unsigned char>(_height >> 8);
+    bmpInfoHeader[10] = static_cast<unsigned char>(_height>> 16);
+    bmpInfoHeader[11] = static_cast<unsigned char>(_height >> 24);
+    bmpInfoHeader[12] = static_cast<unsigned char>(_numberOfColorPlanes);
+    bmpInfoHeader[14] = static_cast<unsigned char>(_bitsPerPixel);
+    return bmpInfoHeader;
+}
+
+
+BmpInfoHeader& BmpInfoHeader::operator=(const BmpInfoHeader& bmpInfoHeader) = default;
