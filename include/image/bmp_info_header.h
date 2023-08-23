7f8b8565843324efb6256a74a0019e9301be3d3b
piroot01 <piroot01@gmail.com>
2023-08-23 21:48:40 +0200

    Edited bmp headers

--- include/image/bmp_info_header.h
+++ include/image/bmp_info_header.h
@@ -2,26 +2,26 @@
 #ifdef BMP_INFO_HEADER_INCLUDED
 
 
+#include "image/bmp_header_size.h"
 #include <cstdint>
+#include <array>
 
 
 class BmpInfoHeader
 {
 public:
-    explicit BmpInfoHeader(uint32_t width, uint32_t height, uint32_t horizontalResolution, uint32_t verticalResolution);
+    explicit BmpInfoHeader(uint32_t width, uint32_t height);
+    std::array<unsigned char, BMP_INFO_HEADER_SIZE> get() const;
+    BmpInfoHeader& operator=(const BmpInfoHeader& bmpInfoHeader);
 
 private:
-    uint32_t _sizeOfThisHeader;
     uint32_t _width;
     uint32_t _height;
     uint16_t _numberOfColorPlanes;
-    uint16_t _colorDepth;
-    uint32_t _compressionMethod;
-    uint32_t _rawBitmapDataSize;
-    uint32_t _horizontalResolution;
-    uint32_t _verticalResolution;
-    uint32_t _colorTableEntries;
-    uint32_t _importantColors;
+    uint32_t _bitsPerPixel;
+
+public:
+    friend class ImageWriter;
 
 };
 
