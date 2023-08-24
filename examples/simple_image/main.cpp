7c01f9b494006e81d8f0ea79e7bf6ea943771af8
piroot01 <piroot01@gmail.com>
2023-08-24 20:41:48 +0200

    Edited example simple_image

--- examples/simple_image/main.cpp
+++ examples/simple_image/main.cpp
@@ -4,15 +4,15 @@
 
 int main()
 {
-    uint32_t width = 16;
-    uint32_t height = 16;
+    uint32_t width = 512;
+    uint32_t height = 512;
     uint32_t pixelNum = width * height;
 
     Image image(width, height); 
     image.generateHeader();
 
     for(uint32_t i = 0; i < pixelNum; ++i)
-        image.appendPixel(Colors::red);
+        image.appendPixel(Colors::black);
 
     return 0;
 }
