d4bef82a1e3bae64c0ed49bae4e21a42338a64f7
piroot01 <piroot01@gmail.com>
2023-08-26 13:14:15 +0200

    Changed the name of bresenham.h

--- include/algorithms/line_algorithm.h
+++ include/algorithms/line_algorithm.h
@@ -13,12 +13,14 @@ class LineAlgorithm : public Algorithm
 {
 public:
     explicit LineAlgorithm(std::shared_ptr<PointSet> pPointSet);
+    void run() = 0;
     virtual void assignParameters(const Point& start, const Point& end) = 0;
-    virtual void assignParameters(const Point& start, const Point& end, const uint32_t lineThickness) = 0;
+//    virtual void assignParameters(const Point& start, const Point& end, const uint32_t lineThickness) = 0;
 
 protected:
     Point _start;
     Point _end;
+    //const uint32_t _lineThickness;
 
 };
 
