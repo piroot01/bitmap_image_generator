d777e7e3a81152ef4d747e6c407c75f8fed5bae1
piroot01 <piroot01@gmail.com>
2023-08-26 13:15:59 +0200

    Added basic methods

--- include/image_objects/drawable_objects_queue.h
+++ include/image_objects/drawable_objects_queue.h
@@ -2,21 +2,33 @@
 #define DRAWABLE_OBJECTS_QUEUE_INCLUDED
 
 
-#include "drawable_objects.h"
+#include "image_objects/drawable_object.h"
 #include <deque>
-
 
 class DrawableObjectsQueue
 {
 public:
     DrawableObjectsQueue();
+    void push(const DrawableObject& drawableObject);
+    void pop();
+    void clear();
 
 private:
-    std::deque<DrawableObjects> _queue;
+    std::deque<DrawableObject> _queue;
 
 };
 
 
+inline void DrawableObjectsQueue::push(const DrawableObject& drawableObject)
+{
+    _queue.push_back(drawableObject);
+}
+
+
+inline void DrawableObjectsQueue::clear()
+{
+    _queue.clear();
+}
 
 
 #endif
