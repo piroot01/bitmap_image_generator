56079c58761b4f076e7fec7f20b8ff47513af778
piroot01 <piroot01@gmail.com>
2023-08-26 13:18:51 +0200

    Make the point public

--- include/image_objects/point.h
+++ include/image_objects/point.h
@@ -11,26 +11,12 @@ public:
     Point();
     Point(const uint32_t x, const uint32_t y);
     Point(const Point& point) = default;
-    uint32_t getX() const;
-    uint32_t getY() const;
 
-private:
-    uint32_t _x;
-    uint32_t _y;
+public:
+    uint32_t x;
+    uint32_t y;
 
 };
-
-
-inline uint32_t Point::getX() const
-{
-    return _x;
-}
-
-
-inline uint32_t Point::getY() const
-{
-    return _y;
-}
 
 
 #endif
