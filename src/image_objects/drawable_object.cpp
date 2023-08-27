0189c2e082e24a76ceb52ce2fd2fca9b5a5e5d0a
piroot01 <piroot01@gmail.com>
2023-08-27 13:17:52 +0200

    Added generate method

--- src/image_objects/drawable_object.cpp
+++ src/image_objects/drawable_object.cpp
@@ -1,12 +1,7 @@
 #include "image_objects/drawable_object.h"
 
 
-DrawableObject::DrawableObject() :
-    _pAlgorithm(nullptr)
-{
-}
-
-DrawableObject::DrawableObject(std::shared_ptr<Algorithm> pAlgorithm) :
+DrawableObject::DrawableObject(const std::shared_ptr<Algorithm> pAlgorithm) :
     _pAlgorithm(pAlgorithm)
 {
 }
