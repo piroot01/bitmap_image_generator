ef5775ff479bc64314c6e3deb4464a472fed6eb5
piroot01 <piroot01@gmail.com>
2023-08-28 11:10:28 +0200

    Added const iterators

--- include/image_objects/drawable_objects_queue.h
+++ include/image_objects/drawable_objects_queue.h
@@ -13,6 +13,11 @@ public:
     void pop();
     void clear();
 
+    std::deque<std::shared_ptr<DrawableObject>>::const_iterator begin() const;
+    std::deque<std::shared_ptr<DrawableObject>>::const_iterator end() const;
+    //std::deque<std::shared_ptr<DrawableObject>>::iterator begin();
+    //std::deque<std::shared_ptr<DrawableObject>>::iterator end();
+
 private:
     std::deque<std::shared_ptr<DrawableObject>> _queue;
 
@@ -28,6 +33,30 @@ inline void DrawableObjectsQueue::push(const std::shared_ptr<DrawableObject> dra
 inline void DrawableObjectsQueue::clear()
 {
     _queue.clear();
+}
+
+
+inline std::deque<std::shared_ptr<DrawableObject>>::const_iterator DrawableObjectsQueue::begin() const
+{
+    return _queue.begin();
+}
+
+
+inline std::deque<std::shared_ptr<DrawableObject>>::const_iterator DrawableObjectsQueue::end() const
+{
+    return _queue.end();
+}
+
+
+inline std::deque<std::shared_ptr<DrawableObject>>::iterator DrawableObjectsQueue::begin()
+{
+    return _queue.begin();
+}
+
+
+inline std::deque<std::shared_ptr<DrawableObject>>::iterator DrawableObjectsQueue::end()
+{
+    return _queue.end();
 }
 
 
