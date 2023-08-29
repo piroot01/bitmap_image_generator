45371e538e01a248c693bcaf9d87ec859d92b48d
piroot01 <piroot01@gmail.com>
2023-08-29 11:50:59 +0200

    Added PointCircularCounterBuffer as an alias

---
+++ include/circular_counter_buffer/point_circular_counter_buffer.h
@@ -0,0 +1,12 @@
+#ifndef POINT_CIRCULAR_COUNTER_BUFFER_INCLUDED
+#define POINT_CIRCULAR_COUNTER_BUFFER_INCLUDED
+
+
+#include "circular_counter_buffer/circular_counter_buffer.h"
+#include "image_objects/point.h"
+
+
+using PointCircularCounterBuffer = CircularCounterBuffer<Point>;
+
+
+#endif
