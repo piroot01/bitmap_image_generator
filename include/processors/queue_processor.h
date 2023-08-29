d9d77dac42d458070b61235ede7b2aea505c3c1e
piroot01 <piroot01@gmail.com>
2023-08-29 11:53:50 +0200

    Support for point collection

--- include/processors/queue_processor.h
+++ include/processors/queue_processor.h
@@ -1,19 +1,21 @@
-#ifndef IMAGE_PROCESSOR_INCLUDED
-#define IMAGE_PROCESSOR_INCLUDED
+#ifndef QUEUE_PROCESSOR_INCLUDED
+#define QUEUE_PROCESSOR_INCLUDED
 
 
 #include "image_objects/drawable_objects_queue.h"
+#include "image_objects/pixel_set.h"
 
 
 class QueueProcessor
 {
 public:
     QueueProcessor();
-    QueueProcessor(const DrawableObjectsQueue& queue);
-    void process() const;
+    QueueProcessor(const DrawableObjectsQueue& queue, const std::shared_ptr<PointCollection> pPointCollection);
+    void process(PixelSet& pixelSet) const;
 
 private:
     const DrawableObjectsQueue* _queue;
+    const std::shared_ptr<PointCollection> _pPointCollection;
 
 };
 
