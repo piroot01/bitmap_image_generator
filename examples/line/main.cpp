fd5e2810e2bedf27e40d7b69b18569972079a81f
piroot01 <piroot01@gmail.com>
2023-08-30 09:41:23 +0200

    Edited line example

--- examples/line/main.cpp
+++ examples/line/main.cpp
@@ -12,16 +12,17 @@ int main()
         Timer t;
         Point start(0, 0);
         Point end(50, 50);
-        std::shared_ptr<PointCollection> pPointCollection = std::make_shared<PointCollection>(30);
+        std::shared_ptr<PointCollection> pPointCollection = std::make_shared<PointCollection>(50);
         std::shared_ptr<BresenhamNormal> pAlgorithm = std::make_shared<BresenhamNormal>(pPointCollection);
         Line line(pAlgorithm, start, end, Colors::black);
         line.generate();
 
+        std::cout << "Counter: " << pPointCollection->getCounter() << ", number of elements: " << pPointCollection->_buffer.size() << '\n';
+        
         int i = 0;
         for (const Point& point : *pPointCollection)
             std::cout << "[" << i++ << "]: " << point.x << ", " << point.y << '\n';
 
-        std::cout << "Counter: " << pPointCollection->getCounter() << '\n';
     }
     return 0;
 }
