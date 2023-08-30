8822135643afc3405c3e113f8599fb74ef312e84
piroot01 <piroot01@gmail.com>
2023-08-30 09:35:50 +0200

    Moved to explicit constructor

--- include/processors/queue_processor.h
+++ include/processors/queue_processor.h
@@ -2,6 +2,7 @@
 #define QUEUE_PROCESSOR_INCLUDED
 
 
+#include "processors/point_set_processor.h"
 #include "image_objects/drawable_objects_queue.h"
 #include "image_objects/pixel_set.h"
 
@@ -9,11 +10,11 @@
 class QueueProcessor
 {
 public:
-    QueueProcessor();
-    QueueProcessor(const DrawableObjectsQueue& queue, const std::shared_ptr<PointCollection> pPointCollection);
+    explicit QueueProcessor(const DrawableObjectsQueue& queue, const std::shared_ptr<PointCollection> pPointCollection);
     void process(PixelSet& pixelSet) const;
 
 private:
+    const PointSetProcessor _pointSetProcessor;
     const DrawableObjectsQueue* _queue;
     const std::shared_ptr<PointCollection> _pPointCollection;
 
