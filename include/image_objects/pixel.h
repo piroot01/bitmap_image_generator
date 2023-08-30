cd367d2320f22e588f67f8170d2958dce05be0bc
piroot01 <piroot01@gmail.com>
2023-08-30 09:35:24 +0200

    Added get method and < operator

--- include/image_objects/pixel.h
+++ include/image_objects/pixel.h
@@ -13,11 +13,14 @@ public:
     Pixel(const Point& point, const Color& color);
     Pixel(const uint32_t x, const uint32_t y, const Color& color);
     const Color& getColor() const;
+    const Point& getPoint() const;
 
 private:
     Point _point;
     const Color& _color;
 
+public:
+    friend bool operator<(const Pixel& lhs, const Pixel& rhs);
 };
 
 
@@ -26,5 +29,16 @@ inline const Color& Pixel::getColor() const
     return _color;
 }
 
+
+inline const Point& Pixel::getPoint() const
+{
+    return _point;
+}
+
+
+inline bool operator<(const Pixel& lhs, const Pixel& rhs)
+{
+    return lhs._point < rhs._point;
+}
 
 #endif
