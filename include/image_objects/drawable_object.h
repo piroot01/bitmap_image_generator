c96dada7e84939b0080a9e0b345e339d318436c8
piroot01 <piroot01@gmail.com>
2023-08-30 09:33:34 +0200

    Added color to drawable object class

--- include/image_objects/drawable_object.h
+++ include/image_objects/drawable_object.h
@@ -3,16 +3,18 @@
 
 
 #include "algorithms/algorithm.h"
+#include "colors/color.h"
 
 
 class DrawableObject
 {
 public:
-    explicit DrawableObject(const std::shared_ptr<Algorithm> algorithm);
+    explicit DrawableObject(const std::shared_ptr<Algorithm> algorithm, const Color& color);
     virtual void generate() const = 0;
 
 protected:
     const std::shared_ptr<Algorithm> _pAlgorithm;
+    const Color& _color;
 
 public:
     friend class QueueProcessor;
