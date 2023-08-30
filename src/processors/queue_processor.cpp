ee5fd387ab16d2a1bbab71f0351acab8e85ee2aa
piroot01 <piroot01@gmail.com>
2023-08-30 09:39:54 +0200

    Edited the queue processor

--- src/processors/queue_processor.cpp
+++ src/processors/queue_processor.cpp
@@ -1,19 +1,18 @@
 #include "processors/queue_processor.h"
+#include "image_objects/pixel_set.h"
 
 
-QueueProcessor::QueueProcessor() = default;
-
-
-QueueProcessor::QueueProcessor(const DrawableObjectsQueue& queue) :
-    _queue(&queue)
+QueueProcessor::QueueProcessor(const DrawableObjectsQueue& queue, const std::shared_ptr<PointCollection> pPoinCollection) :
+    _pointSetProcessor(pPoinCollection), _queue(&queue), _pPointCollection(pPoinCollection)
 {
 }
 
 
-void QueueProcessor::process() const
+void QueueProcessor::process(PixelSet& pixelSet) const
 {
     for (const auto& iterator : *_queue)
     {
         iterator->generate();
+        _pointSetProcessor.process(pixelSet, iterator->_color);
     }
 }
