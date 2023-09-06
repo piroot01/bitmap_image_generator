81978885be436f647fd4e7e2515d1b08d9921ba5
piroot01 <piroot01@gmail.com>
2023-09-06 23:01:23 +0200

    Implemented line check

--- include/image_objects/line.h
+++ include/image_objects/line.h
@@ -5,14 +5,17 @@
 #include "image_objects/drawable_object.h"
 #include "image_objects/point.h"
 #include "colors/color.h"
+#include "exception/exception.h"
 
 
 class Line : public DrawableObject
 {
 public:
     explicit Line(const std::shared_ptr<Algorithm> algorithm, const Point& start, const Point& end, const Color& color);
+    explicit Line(const std::shared_ptr<Algorithm> algorithm, const Point&& start, const Point&& end, const Color& color);
     explicit Line(const std::shared_ptr<Algorithm> algorithm, const LineParameters& parameters, const Color& color);
     void generate() const override;
+    void check(const uint32_t imageWidth, const uint32_t imageHeight) const override;
 
 private:
     const LineParameters _parameters;
@@ -25,5 +28,14 @@ inline void Line::generate() const
     _pAlgorithm->run(&_parameters);
 }
 
+
+inline void Line::check(const uint32_t imageWidth, const uint32_t imageHeight) const
+{
+    if (_parameters._start.x >= imageWidth or
+        _parameters._start.y >= imageHeight or
+        _parameters._end.x >= imageWidth or
+        _parameters._end.y >= imageWidth)
+        throw InvalidArgumentException("The line endpoints are not within the image boundaries.");
+}
 
 #endif
