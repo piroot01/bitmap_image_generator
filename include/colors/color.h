0a7d22bc669cf39d429a2bd9a691134642b76499
piroot01 <piroot01@gmail.com>
2023-09-06 18:19:31 +0200

    Make the Color default constructable

--- include/colors/color.h
+++ include/colors/color.h
@@ -8,7 +8,7 @@
 class Color
 {
 public:
-    explicit Color(uint8_t red, uint8_t green, uint8_t blue);
+    explicit Color(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0);
     uint8_t blue;
     uint8_t green;
     uint8_t red;
