17536d7d86a2c4f93ad28d167be9d9133e24de38
piroot01 <piroot01@gmail.com>
2023-09-06 23:04:41 +0200

    Edited implementation of PointSetProcessor

--- src/processors/point_set_processor.cpp
+++ src/processors/point_set_processor.cpp
@@ -1,20 +1,17 @@
 #include "processors/point_set_processor.h"
 
 
-PointSetProcessor::PointSetProcessor(const std::shared_ptr<PointCollection> pPointCollection) :
-    _pPointCollection(pPointCollection)
-{
-}
+PointSetProcessor::PointSetProcessor() = default;
 
 
-void PointSetProcessor::process(PixelSet& pixelSet, const Color& color) const
+void PointSetProcessor::process(PixelSet& pixelSet, PointCollection* pPointCollection, const Color& color) const
 {
-    auto iterator = _pPointCollection->rbegin();;
-    uint32_t count = _pPointCollection->getCounter();
+    auto iterator = pPointCollection->rbegin();;
+    uint32_t count = pPointCollection->getCounter();
     for (uint32_t i = 0; i < count; ++i)
     {
         pixelSet.emplace(*iterator, color);
         iterator++;
     }
-    _pPointCollection->resetCounter();
+    pPointCollection->resetCounter();
 }
