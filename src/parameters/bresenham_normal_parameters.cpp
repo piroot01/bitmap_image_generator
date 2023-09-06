4d8ae0fc15b163b6d9255136378a72dd7ac485f9
piroot01 <piroot01@gmail.com>
2023-09-06 18:25:48 +0200

    Edited includes

---
+++ src/parameters/bresenham_normal_parameters.cpp
@@ -0,0 +1,7 @@
+#include "parameters/bresenham_normal_parameters.h"
+
+
+BresenhamNormalParameters::BresenhamNormalParameters(const Point& start, const Point& end) :
+    LineParameters(start, end)
+{
+}
---
+++ src/parameters/line_parameters.cpp
@@ -0,0 +1,7 @@
+#include "parameters/line_parameters.h"
+
+
+LineParameters::LineParameters(const Point& start, const Point& end) :
+    _start(start), _end(end)
+{
+}
